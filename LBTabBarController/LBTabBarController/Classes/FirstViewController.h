5d826f0bb050c85a10f20245fc3fb053a49fe972
kingly <362108564@qq.com>
2016-02-27 18:10:31 +0800

    优化

--- LBTabBarController/LBTabBarController/Classes/FirstViewController.h
+++ LBTabBarController/LBTabBarController/Classes/FirstViewController.h
@@ -2,7 +2,7 @@
 //  FirstViewController.h
 //  LiBTabBarController
 //
-//  Created by kingly on 15/12/11.
+//  Created by kingly https://github.com/kingly09/LBinTabBarController  on 15/12/11.
 //  Copyright © 2015年 kingly. All rights reserved.
 //
 
--- LBTabBarController/LBTabBarController/LBTab/LBTabBarController.m
+++ LBTabBarController/LBTabBarController/LBTab/LBTabBarController.m
@@ -2,7 +2,7 @@
 //  LBTabBarController.m
 //  LBTabBarController
 //
-//  Created by kingly on 15/12/11.
+//  Created by kingly https://github.com/kingly09/LBinTabBarController  on 15/12/11.
 //  Copyright © 2015年 kingly. All rights reserved.
 //
 
